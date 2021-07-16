7d84f612955a490affd8b39b96dba53cf21be12c
Exunys <76539058+Exunys@users.noreply.github.com>
2021-07-16 15:05:06 +0200

    Script

---
+++ Anti Kick.lua
@@ -0,0 +1,39 @@
+--// Variables
+
+local Players = game:GetService("Players")
+local GameMetaTable = getrawmetatable(game)
+local OldNameCall = nil
+
+--// Global Variables
+
+getgenv().SendNotifications = true -- Set to true if you want to get notified regularly.
+
+--// Anti Kick Hook
+
+OldNameCall = hookmetamethod(game, "__namecall", function(Self, ...)
+    local NameCallMethod = getnamecallmethod()
+
+    if tostring(string.lower(NameCallMethod)) == "kick" then
+        if getgenv().SendNotifications == true then
+            game:GetService("StarterGui"):SetCore("SendNotification", {
+                Title = "Exunys Developer",
+                Text = "You almost got kicked! Successfully prevented.",
+                Icon = "rbxassetid://6238540373",
+                Duration = 3,
+            })
+        end
+        
+        return nil
+    end
+    
+    return OldNameCall(Self, ...)
+end)
+
+if getgenv().SendNotifications == true then
+    game:GetService("StarterGui"):SetCore("SendNotification", {
+        Title = "Exunys Developer",
+        Text = "Anti-Kick script loaded",
+        Icon = "rbxassetid://6238537240",
+        Duration = 5,
+    })
+end
